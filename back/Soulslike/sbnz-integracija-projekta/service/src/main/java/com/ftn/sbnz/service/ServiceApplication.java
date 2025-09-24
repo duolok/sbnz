07c22585003b3c09f477f54515075ac7fd2fff89
duolok <dusan.lecic5@gmail.com>
2025-09-24 22:30:39 +0200

    add: starter
    Signed-off-by: duolok <dusan.lecic5@gmail.com>

--- back/Soulslike/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
+++ back/Soulslike/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/ServiceApplication.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieScanner;
 import org.kie.api.runtime.KieContainer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ftn.sbnz.service", "com.ftn.sbnz.model"})
+@EntityScan(basePackages = {"com.ftn.sbnz.model.models"})
+@EnableJpaRepositories(basePackages = {"com.ftn.sbnz.service"})
 public class ServiceApplication  {
 	
 	private static Logger log = LoggerFactory.getLogger(ServiceApplication.class);
